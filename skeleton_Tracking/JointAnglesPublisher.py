3f85d235791396e7bb980ce088dfba5023411992
NayanSpace <nayanbasin@gmail.com>
2024-02-03 19:11:39 -0500

    ffixes

--- skeleton_Tracking/JointAnglesPublisher.py
+++ skeleton_Tracking/JointAnglesPublisher.py
@@ -15,16 +15,10 @@ class JointAnglesPublisher:
             normalized_tuple = tuple(angle % 360.0 for angle in angle_tuple)
             normalized_angles.append(normalized_tuple)
 
-        # Create a Float32MultiArray message
         msg = Float32MultiArray()
-
-        # Flatten the list of tuples to create a 1D list
         flat_normalized_angles = [angle for angle_tuple in normalized_angles for angle in angle_tuple]
-
-        # Assign the normalized angles to 'data'
         msg.data = flat_normalized_angles
 
-        # Publish the message
         self.publisher.publish(msg)
         
     def spin(self):
